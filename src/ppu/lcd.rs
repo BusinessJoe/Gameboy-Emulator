fce772d47011060c43142263d81b66d663aa19a9
Lukas Boelling <lukasboelling002@gmail.com>
2023-04-02 16:59:17 -0400

    Fix lcd control read bug

--- src/ppu/lcd.rs
+++ src/ppu/lcd.rs
@@ -35,13 +35,13 @@ impl LcdControl {
     }
 
     pub fn read(&self) -> u8 {
-        (self.bg_window_enable as u8) + (self.obj_enable as u8)
-            << 1 + (self.obj_size as u8)
-            << 2 + (self.bg_tile_map_area as u8)
-            << 3 + (self.bg_window_tile_data_area as u8)
-            << 4 + (self.window_enable as u8)
-            << 5 + (self.window_tile_map_area as u8)
-            << 6 + (self.lcd_ppu_enable as u8)
+        (self.bg_window_enable as u8) | (self.obj_enable as u8)
+            << 1 | (self.obj_size as u8)
+            << 2 | (self.bg_tile_map_area as u8)
+            << 3 | (self.bg_window_tile_data_area as u8)
+            << 4 | (self.window_enable as u8)
+            << 5 | (self.window_tile_map_area as u8)
+            << 6 | (self.lcd_ppu_enable as u8)
             << 7
     }
 
